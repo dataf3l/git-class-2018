f868d46a8834dbee725c8f663856256204a1335c
Felipe@MBP2 <auditor400@gmail.com>
2018-11-24 18:41:18 -0500

    Made a new function called Maybe()

--- program.py
+++ program.py
@@ -1,10 +1,14 @@
 print("123")
+
+def Maybe():
+    return False
+
 print("123")
 print("123")
 print("123")
 print("123")
 print("123")
-if True:
+if Maybe:
     print("123")
 print("123")
 print("123")
